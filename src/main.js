fe0c8bec7a05d282fa3b416ca4bd2a3adc97e2f5
sinhexe <sinhexe@gmail.com>
2022-05-07 16:02:23 +0700

    create routes, app/controllers

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ const { engine } = require ('express-handlebars');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
+//routes
+
+const route = require('./routes/index');
+
 //static files
 
 app.use(express.static(path.join(__dirname,'public')));
@@ -26,29 +30,10 @@ app.engine('hbs', engine({
 app.set('view engine','hbs');
 app.set('views', path.join(__dirname,'resource/views')); //config views path
 
-//route
+//route init
 
-//---------GET------------//
+route(app);
 
-//hostname/
-app.get('/', (req, res) => {
-  return res.render('home');
-});
-//hostname/news
-app.get('/news', (req, res) => {
-  return res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  return res.render('search');
-});
-
-//---------POST------------//
-
-//hostname/search
-app.post('/search', (req,res) =>{
-  return res.send(req.body);
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const newsRouter = require('./news.route');
+const siteRouter = require('./site.route');
+
+function route(app) {
+
+    //---------GET------------//
+
+   
+    //hostname/news
+    app.use('/news',newsRouter);
+    
+    //hostname
+    app.use('/',siteRouter);
+
+
+}
+
+module.exports = route;
